b9358c99e80cd3ce50c1f218c4b80a580c744115
hammarhank <axtr@live.com>
2023-03-02 09:38:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProcessStartInfo z = new ProcessStartInfo();
+        z.FileName = "zenity.exe";
+        z.Arguments = "--forms --title= \"Add a Link\" --text=\"Forms Dialog\" --add-entry=\"Link\" --add-entry=\"Info\" --add-entry=\"URL\"";
+        z.WorkingDirectory = @"C:\Program Files (x86)\Zenity\bin\";
+        z.RedirectStandardOutput = true;
+        z.RedirectStandardError = true;
+        z.UseShellExecute = false;
+
+
+        using (Process process = Process.Start(z))
+        {
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            // Skriv ut formuläret på konsolen
+            Console.WriteLine(output);
+
+            // Läs indata från användaren från konsolen
+            string link = Console.ReadLine();
+            string info = Console.ReadLine();
+            string url = Console.ReadLine();
+
+            // Skriv ut den inmatade datan på konsolen
+            Console.WriteLine($"Link: {link}");
+            Console.WriteLine($"Info: {info}");
+            Console.WriteLine($"URL: {url}");
+        }
+
+
+    }
+}
